ef0db2fecf8897fee0755ae30b4e9cd29621f927
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-02-22 22:37:27 +0900

    MySQL接続情報などをDIコンテナへ登録する処理を追加

--- AgriNaviApi.API/Program.cs
+++ AgriNaviApi.API/Program.cs
@@ -1,4 +1,16 @@
+using AgriNaviApi.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// appsettings.jsonからデフォルトのDB接続文字列を取得
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var mySqlVersionString = builder.Configuration.GetValue<string>("DatabaseOptions:MySqlVersion");
+
+// MySQLバージョン変換
+Version mySqlVersion;
+mySqlVersion = Version.Parse(mySqlVersionString);
+var serverVersion = new MySqlServerVersion(mySqlVersion);
 
 // Add services to the container.
 
@@ -6,6 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, serverVersion));
 
 var app = builder.Build();
 
