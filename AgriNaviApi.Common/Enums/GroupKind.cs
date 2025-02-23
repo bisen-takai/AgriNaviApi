b5ca6e9b622be5ac85ff0e0f9a9b3646c9b56da3
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-02-23 14:39:11 +0900

    グループ名テーブルのエンティティを新規作成

---
+++ AgriNaviApi.Common/Enums/GroupKind.cs
@@ -0,0 +1,28 @@
+﻿namespace AgriNaviApi.Common.Enums
+{
+    /// <summary>
+    /// グループ名の種別
+    /// </summary>
+    public enum GroupKind
+    {
+        /// <summary>
+        /// 未定義(デフォルト)
+        /// </summary>
+        Undefined = 0,
+
+        /// <summary>
+        /// 圃場情報グループ
+        /// </summary>
+        Farm = 1,
+
+        /// <summary>
+        /// 作付名グループ
+        /// </summary>
+        Planting = 2,
+
+        /// <summary>
+        /// その他グループ
+        /// </summary>
+        Other = 3
+    }
+}
---
+++ AgriNaviApi.Infrastructure/Persistence/Entities/GroupPersistenceEntity.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using AgriNaviApi.Common.Enums;
+
+namespace AgriNaviApi.Infrastructure.Persistence.Entities
+{
+    /// <summary>
+    /// グループ名テーブル
+    /// </summary>
+    [Table("groups")]
+    public class GroupPersistenceEntity
+    {
+        /// <summary>
+        /// グループ名ID(自動インクリメントID)
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("group_id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// グループ名UUID
+        /// </summary>
+        [Column("group_uuid")]
+        public Guid Uuid { get; set; }
+
+        /// <summary>
+        /// グループ名
+        /// </summary>
+        [Column("group_name")]
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// グループ種別
+        /// </summary>
+        [Column("group_kind")]
+        public GroupKind Kind { get; set; }
+
+        /// <summary>
+        /// 削除フラグ
+        /// </summary>
+        [Column("group_delete_flg")]
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// 登録日時
+        /// </summary>
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// 最終更新日時
+        /// </summary>
+        [Column("last_updated_at")]
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
