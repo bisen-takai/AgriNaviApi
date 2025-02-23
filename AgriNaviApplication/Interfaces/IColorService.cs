beac39ef5f3b1f84a6ebdc6e1d732b71ca0dc1eb
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-02-23 19:32:43 +0900

    テストのしやすさを考慮して、サービス層のインタフェースを実装(カラーテーブルに関するサービスのみ)

---
+++ AgriNaviApplication/Interfaces/IColorService.cs
@@ -0,0 +1,39 @@
+﻿using AgriNaviApi.Application.DTOs.Colors;
+using AgriNaviApi.Application.Requests.Colors;
+
+namespace AgriNaviApi.Application.Interfaces
+{
+    /// <summary>
+    /// カラーテーブルに関するサービス処理
+    /// </summary>
+    public interface IColorService
+    {
+        /// <summary>
+        /// カラーテーブルに登録する
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<ColorCreateDto> CreateColorAsync(ColorCreateRequest request);
+
+        /// <summary>
+        /// カラーテーブルを更新する
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<ColorUpdateDto> UpdateColorAsync(ColorUpdateRequest request);
+
+        /// <summary>
+        /// カラーテーブルから削除する
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<ColorDeleteDto> DeleteColorAsync(ColorDeleteRequest request);
+
+        /// <summary>
+        /// カラーテーブルを検索する
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<ColorSearchDto> SearchColorAsync(ColorSearchRequest request); 
+    }
+}
