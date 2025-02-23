4e42da3f3c1c3a87555e3f35c569323ca0f0d46b
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-02-23 22:19:40 +0900

    UUIDを生成する共通処理を新規追加

---
+++ AgriNaviApi.Common/Interfaces/IUuidGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace AgriNaviApi.Common.Interfaces
+{
+    /// <summary>
+    /// UUIDを生成するインタフェース
+    /// </summary>
+    public interface IUuidGenerator
+    {
+        /// <summary>
+        /// UUIDを生成する
+        /// </summary>
+        /// <returns>UUID</returns>
+        Guid GenerateUuid();
+    }
+}
---
+++ AgriNaviApi.Common/Utilities/UuidGenerator.cs
@@ -0,0 +1,19 @@
+﻿using AgriNaviApi.Common.Interfaces;
+
+namespace AgriNaviApi.Common.Utilities
+{
+    /// <summary>
+    /// UUIDを生成する
+    /// </summary>
+    public class UuidGenerator : IUuidGenerator
+    {
+        /// <summary>
+        /// UUIDを生成する
+        /// </summary>
+        /// <returns>UUID</returns>
+        public Guid GenerateUuid()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
