6a34258ee48d81f922f8f82725fd9eb23edeba4b
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-02-24 18:03:26 +0900

    ColorServiceと、ColorProfileのDIコンテナ登録

---
+++ AgriNaviApplication/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AgriNaviApi.Application.Interfaces;
+using AgriNaviApi.Application.Profiles;
+using AgriNaviApi.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgriNaviApi.Application.Extensions
+{
+    /// <summary>
+    /// Application層のサービスを登録する拡張メソッド
+    /// </summary>
+    public static class ApplicationServiceExtensions
+    {
+        /// <summary>
+        /// Application層のサービスを登録する
+        /// </summary>
+        /// <param name="services">builder.Services</param>
+        /// <returns>builder.Services</returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IColorService, ColorService>();
+            services.AddAutoMapper(typeof(ColorProfile).Assembly);
+
+            return services;
+        }
+    }
+}
