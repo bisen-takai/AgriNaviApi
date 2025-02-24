7c495523866b6ded1813b390a4a1dcb62d19c50e
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-02-24 18:02:07 +0900

    重複登録の例外を明示するための定義

---
+++ AgriNaviApi.Common/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,13 @@
+﻿namespace AgriNaviApi.Common.Exceptions
+{
+    /// <summary>
+    /// 登録済みの例外
+    /// </summary>
+    /// <remarks>
+    /// 重複登録の例外を明示することが目的のため、特に処理は無し
+    /// </remarks>
+    public class DuplicateEntityException : Exception
+    {
+        public DuplicateEntityException(string message) : base(message) { }
+    }
+}
