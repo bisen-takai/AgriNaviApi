e93bb33c8aab9808d679658c485c42406a8ecea4
hidetomo.takai <h_takai_0604@bisenblog.com>
2025-03-28 23:21:53 +0900

    Entityのユニットテスト後のマイグレーション

---
+++ AgriNaviApi.Infrastructure/Migrations/20250328141733_UnitTestAfter.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriNaviApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UnitTestAfter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_address",
+                table: "users",
+                type: "varchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "field_short_name",
+                table: "fields",
+                type: "varchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldMaxLength: 4)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "crop_short_name",
+                table: "crops",
+                type: "varchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldMaxLength: 4)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_address",
+                table: "users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "fields",
+                keyColumn: "field_short_name",
+                keyValue: null,
+                column: "field_short_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "field_short_name",
+                table: "fields",
+                type: "varchar(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "crops",
+                keyColumn: "crop_short_name",
+                keyValue: null,
+                column: "crop_short_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "crop_short_name",
+                table: "crops",
+                type: "varchar(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
